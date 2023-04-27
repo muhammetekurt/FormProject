217f0555d3e2abb0d998e300ee0a3dd998811bc6
MUHAMMET EMİN KURT <mekurt55@hotmail.com>
2023-04-27 15:00:54 +0300

    edit and delete form added

--- FormProject/Controllers/FormController.cs
+++ FormProject/Controllers/FormController.cs
@@ -30,12 +30,77 @@ namespace FormProject.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Form form)
         {
-            // form verilerini veri tabanına kaydedin
-            // burada veri tabanı işlemleri yapılır
-            // örneğin veri tabanına kaydettikten sonra Index sayfasına yönlendirin
-            _context.Forms.Add(form);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _context.Forms.Add(form);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(form);
         }
-    }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _context.Forms.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Form obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _context.Forms.Update(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var categoryFromDb = _context.Forms.Find(id);
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+
+			return View(categoryFromDb);
+		}
+
+		//POST
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePOST(int? id)
+		{
+			var obj = _context.Forms.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			_context.Forms.Remove(obj);
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+
+		}
+	}
 }
